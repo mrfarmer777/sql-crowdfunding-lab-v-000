e6bb26c6ee7c0d04a4c2498f8a8180239259ffd3
Matt Farmer <mrfarmer777@gmail.com>
2018-06-09 20:55:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,27 @@
+INSERT INTO users(name,age) VALUES
+("Pickles",25),
+("Larry",25),
+("Curly",25),
+("Moe",25),
+("Pickles",28),
+("Pickles",25),
+("Pickles",35),
+("Jane",72),
+("Sally",65),
+("Greta",25),
+("Igor",48),
+("Isaac",29),
+("Stan",25),
+("O'Flaherty",18),
+("June",25),
+("Alex",25),
+("Portia",25),
+("Aggie",25),
+("Magge",25),
+("Flaggie",25),
+("Moonbeam",25);
+
+
 INSERT INTO projects(title,category,funding_goal,start_date,end_date) VALUES
 ("pickles","food",104000,"3/25/15","3/25/18"),
 ("tacos","food",10040,"3/25/15","3/25/18"),
