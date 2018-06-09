793b57649767514c488a475b1dfe64044a05d464
Matt Farmer <mrfarmer777@gmail.com>
2018-06-09 20:51:35 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects(title,category,funding_goal,start_date,end_date) VALUES 
+("pickles","food",104000,"3/25/15","3/25/18"),
+("tacos","food",10040,"3/25/15","3/25/18"),
+("anchovies","food",10000,"3/25/15","3/25/18"),
+("computers","tech",10000,"3/25/15","3/25/18"),
+("laser eyes","tech",10000,"3/25/15","3/25/18"),
+("teleportation","tech",10000,"3/25/15","3/25/18"),
+("moon landing","space travel",10000,"3/25/15","3/25/18"),
+("jovian Mc Donald's","space food",10000,"3/25/15","3/25/18"),
+("Apple Crisp","food",10000,"3/25/15","3/25/18"),
+("horseback flamethrower","tech",10000,"3/25/15","3/25/18");
